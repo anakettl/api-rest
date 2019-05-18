03242b8c16055191643a55f78e6652af474050a2
ALEXSANDRO FERNANDES SARTORI ROLIANO <ALU0810510@uniritter.local>
2019-05-18 16:57:52 -0300

    Cria service

--- src/main/java/com/anakettl/APIREST/repositories/ContactRepository.java
+++ src/main/java/com/anakettl/APIREST/repositories/ContactRepository.java
@@ -1,5 +1,11 @@
 package com.anakettl.APIREST.repositories;
 
-public interface ContactRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.anakettl.APIREST.models.Contact;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long>{
 
 }
---
+++ src/main/java/com/anakettl/APIREST/services/ContactService.java
@@ -0,0 +1,8 @@
+package com.anakettl.APIREST.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactService {
+
+}
